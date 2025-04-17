17675abe5d39294fb61bd9c1c82d2f258856bbf8
기택 <samscientist@naver.com>
2025-04-17 19:26:43 +0900

    feat: setting up Starlight (math, diagram)
    Add features for being able to write LaTeX math formulas and Mermaid diagrams.
    Along with documentation on how to implement it.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import rehypeMathJax from 'rehype-mathjax';
+import remarkMath from 'remark-math';
+import remarkMermaid from 'remark-mermaidjs';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [remarkMath, remarkMermaid],
+		rehypePlugins: [rehypeMathJax],
+	},
+
 	integrations: [
 		starlight({
 			title: 'COMB',
+			customCss: ['./src/mathjax.css'],
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/candleboxyz' },
 				{ icon: 'linkedin', label: 'LinkedIn', href: '' }
@@ -26,6 +35,10 @@ export default defineConfig({
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
+				},
+				{
+					label: 'memo',
+					autogenerate: {directory: 'memo'}
 				},
 			],
 		}),
